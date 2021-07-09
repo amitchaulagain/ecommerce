9093c4d70f0fddae41ea5f0f94c8fe3069bd105e
Abik Mushyakho <abik.mushyakho94@gmail.com>
2021-07-09 22:07:40 +0545

    front list show

--- src/main/java/com/ismt/repository/ProductRepo.java
+++ src/main/java/com/ismt/repository/ProductRepo.java
@@ -67,6 +67,37 @@ public class ProductRepo {
         }
         return products;
     }
+    public List<Product> listProductForFrontEnd() {
+
+        final String QUERY = "select * from product ";
+
+        // using try-with-resources to avoid closing resources (boiler plate code)
+
+        // Step 1: Establishing a Connection
+        List<Product> products = new ArrayList<>();
+        try (Connection connection = JDBCUtils.getConnection();
+
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(QUERY);) {
+            // preparedStatement.setInt(1, 1);
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Step 4: Process the ResultSet object.
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String description = rs.getString("description");
+                Double price = rs.getDouble("price");
+                System.out.println(id + "," + name + "," + description+" "+price);
+                products.add(new Product(id, name, description,price));
+            }
+        } catch (SQLException e) {
+            JDBCUtils.printSQLException(e);
+        }
+        return products;
+    }
 
     public void delete(int id) {
         final String QUERY = "delete from product where id=? ";
